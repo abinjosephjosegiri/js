59e0bd9e1dfe6ea0e8e9421a59b43dc74d2910aa
Abin Joseph <abjs@pop-os.localdomain>
2020-10-23 23:40:10 +0530

    printingData

---
+++ 2_printingData/console.log.js
@@ -0,0 +1,23 @@
+// simple hello world 
+console.log("Hello Word");
+// Number
+console.log(28);
+// Number and String
+
+console.log("sum of" + 2 +" and " + 8 + " is 10" )
+
+let number =10;
+
+//verable
+console.log(number);
+
+//verable with text
+
+console.log("Number is " + number)
+
+let a =20 ;
+let b =30 ;
+let sum =a+b;
+console.log("Sum of " + a + " and " + b + " is " + sum);
+// or
+console.log(`sum of ${a} and ${b} is ${sum}`);
