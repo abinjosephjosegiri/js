a5f37fd62c80d3199c453692b8a20d209e43568a
Abin Joseph <abjs@pop-os.localdomain>
2020-10-24 01:13:08 +0530

    LogIn

---
+++ login.js
@@ -0,0 +1,17 @@
+// Allow user to access the website
+// login from email
+// login from Google account
+// login from facebook account
+
+let email = false;
+let facebook =false;
+let google =false;
+
+if (email || facebook || google )
+{
+    console.log ('LogIn Sussessfull');
+}
+else
+{
+    console.log('LogIn Failed')
+}
